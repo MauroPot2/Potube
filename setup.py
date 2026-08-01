62940c0bc0952f10623b62c53c27522cb02c6043
Mauro Pot <120337162+MauroPot2@users.noreply.github.com>
2026-08-01 15:54:15 +0200

    update package metadata

--- setup.py
+++ setup.py
@@ -2,23 +2,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='potube',
-    version='1.0.0',
-    description='Scarica e converti video YouTube in MP3',
-    author='Mauro Potestio',
-    author_email='tuo@email.it',
+    name="potube",
+    version="1.1.0",
+    description="Scarica audio da YouTube e convertilo in MP3 tramite yt-dlp",
+    author="Mauro Potestio",
     packages=find_packages(),
-    install_requires=[
-        'pytubefix'
-    ],
+    install_requires=[],
     entry_points={
-        'console_scripts': [
-            'potube = potube.__main__:main'
+        "console_scripts": [
+            "potube = potube.__main__:main",
         ],
     },
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.9',
+    python_requires=">=3.9",
 )
